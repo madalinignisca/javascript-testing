02919c44b0f5465a709807462a695e9e7ed72504
Madalin Ignisca <619561+madalinignisca@users.noreply.github.com>
2018-08-02 23:59:54 +0200

    Play some more with basic toBe and toBeClose tests

--- sum.test.js
+++ sum.test.js
@@ -7,3 +7,7 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test ('adds 7.12 + 8.65 to be equal to 15.77', () => {
+    expect(sum(7.12, 8.65)).toBeCloseTo(15.77);
+});

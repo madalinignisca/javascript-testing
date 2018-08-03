729b86cde859c558808249764adace0672658c42
Madalin Ignisca <619561+madalinignisca@users.noreply.github.com>
2018-08-03 14:13:15 +0200

    Install and init Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-    "extends": "standard"
+    "plugins": [
+        "jest"
+    ],
+    "extends": [
+        "standard",
+        "plugin:jest/recommended"
+    ]
 };
--- difference.test.js
+++ difference.test.js
@@ -8,6 +8,6 @@ test('substracting  3 from 7 must equal 4', () => {
     expect(difference(7, 3)).toBe(4);
 });
 
-test ('substracting 7.12 from 8.65 to be close to 1.53', () => {
+test('substracting 7.12 from 8.65 to be close to 1.53', () => {
     expect(difference(8.65, 7.12)).toBeCloseTo(1.53);
 });
